02583fa081e3b137552eb3271e65fbfb66abd206
Matt <Guarrdon@hotmail.com>
2019-09-01 11:10:25 -0400

    Updated default port to 8000 for Openshift deploy

--- loanApprovalService/src/server.js
+++ loanApprovalService/src/server.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(require("./routes"));
 
 //start the server listening on a port
-const port = process.env.LOANAPPROVAL_SERVICE_PORT || 80;
+const port = process.env.LOANAPPROVAL_SERVICE_PORT || 8000;
 app.listen(port, () => {
     console.log(`LoanApprovalService is listening on port ${port}`);
 });
