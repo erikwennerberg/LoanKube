a0b3fd24570d7c7c17aa3f45c96a64a399f40360
matt <matt@sneaka.io>
2020-02-26 08:02:19 -0500

    updates to initi script

--- loanDataService-Postgres/init/InitDatabase.sql
+++ loanDataService-Postgres/init/InitDatabase.sql
@@ -1,3 +1,5 @@
+
+
 SELECT a.approved, r.rejected, t.total, (t.total-(a.approved+r.rejected)) as notprocessed
 FROM (SELECT count(*) as approved
     FROM applications
@@ -8,4 +10,6 @@ FROM (SELECT count(*) as approved
     (SELECT count(*) as total
     FROM applications) as t
 
+\connect loankube
 CREATE TABLE applications (application_id varchar(50) PRIMARY KEY, loan_data VARCHAR (512) UNIQUE NOT NULL, application_status varchar(10), modified TIMESTAMP NULL);
+GRANT ALL PRIVILEGES ON TABLE applications TO loankube;
