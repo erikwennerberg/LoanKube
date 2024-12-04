ed484f331c7bac313496c1496a3e5bcd260aba2f
Erik Wennerberg <erikwennerberg@eriks-mbp.lan>
2024-12-04 16:15:55 -0500

    fix OTEL exporter

--- loanDataService-Postgres/src/instrumentation.js
+++ loanDataService-Postgres/src/instrumentation.js
@@ -14,9 +14,13 @@ const {
 
 
 const sdk = new NodeSDK({
-  traceExporter: new OTLPTraceExporter(),
+  traceExporter: new OTLPTraceExporter({
+    url: 'http://localhost:4318/v1/traces',
+  }),
   metricReader: new PeriodicExportingMetricReader({
-    exporter: new OTLPMetricExporter(),
+    exporter: new OTLPMetricExporter({
+      url: 'http://localhost:4318/v1/metrics'
+    })
   }),
   instrumentations: [getNodeAutoInstrumentations()],
 });
